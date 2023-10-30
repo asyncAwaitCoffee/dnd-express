c1c0e85f62cb2dd75ac9e42fd50f224fe0d357c0
Andrei Stepanov <steam.oni.am@gmail.com>
2023-10-30 19:58:06 +0400

    minor corrections

--- models/DB.js
+++ models/DB.js
@@ -1,7 +1,9 @@
 import pg from "pg"
 import {DBException} from "./Wrappers.js"
 //import { host, port, database, user, password } from "../config/config.js"
-import { host, port, database, user, password } from "/etc/secrets/config.js"
+//import { host, port, database, user, password } from "/etc/secrets/config.js"
+import pkg from '/etc/secrets/config.js';
+const { host, port, database, user, password } = pkg;
 
 const { Pool } = pg;
 
